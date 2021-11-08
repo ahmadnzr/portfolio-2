eba15ac8b8df26fd5906b77ec82ba8c29007cf4e
Ahmad Nizar <ahmad.rpl001@gmail.com>
2021-11-08 17:22:46 +0700

    product components

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,25 @@
+import Product from "../product/Product";
+import "./productList.css";
+import { products } from "../../data";
+
+const ProductList = () => {
+  return (
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">Lorem, ipsum dolor.</h1>
+        <p className="pl-desc">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+          consequuntur sed, odio corrupti placeat impedit autem ea natus magnam
+          quos!
+        </p>
+      </div>
+      <div className="pl-list">
+        {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://raw.githubusercontent.com/ahmadnzr/myimg/main/1/1.jpg",
+    link: "",
+  },
+  {
+    id: 2,
+    img: "https://raw.githubusercontent.com/ahmadnzr/myimg/main/1/2.jpg",
+    link: "",
+  },
+  {
+    id: 3,
+    img: "https://raw.githubusercontent.com/ahmadnzr/myimg/main/1/3.jpg",
+    link: "",
+  },
+  {
+    id: 4,
+    img: "https://raw.githubusercontent.com/ahmadnzr/myimg/main/1/4.jpg",
+    link: "",
+  },
+  {
+    id: 5,
+    img: "https://raw.githubusercontent.com/ahmadnzr/myimg/main/1/5.jpg",
+    link: "",
+  },
+  {
+    id: 6,
+    img: "https://raw.githubusercontent.com/ahmadnzr/myimg/main/1/6.jpg",
+    link: "",
+  },
+];
